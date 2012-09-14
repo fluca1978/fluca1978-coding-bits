d4cafbc18d74a9b0869b9fed0d0ef845264f103d
Luca Ferrari <fluca1978@gmail.com>
2012-09-14 08:40:59 +0200

    Added the merge sort. Refactored each sorting method to have the same prototype so that a single run_sort function can be used to run and report results about any alghoritm. Moreover, the run_sort function now makes a clone of the array to sort, so that each alghoritm works on its private copy and therefore the same set of data is used for all the alghoritms.

--- teaching-stuff/c/sorting/sorting_main.c
+++ teaching-stuff/c/sorting/sorting_main.c
@@ -2,6 +2,48 @@
 /**
  * This is a simple program to show how the different main sorting
  * alghortims work.
+ *
+ * To compile the program use the following command:
+
+ gcc -DSORT_VERBOSE -lm -g -o sort.exe sorting_main.c
+
+ * where the SORT_VERBOSE flag indicates to produce output at each step of sorting.
+ * It is also possible to use the SORT_SIMPLE flag to produce a very simple
+ * initial array to ease the alghortim analysys.
+ *
+ *
+ *
+ * The following is an example of a program output:
+ 
+ Welcome to the sorting sample!
+
+The program is configured for sorting max 10000 random values
+
+#######################################
+
+		Bubble Sort O(n^2) = 100000000 
+
+
+		Bubble Sort took 1.04 secs, complexity was 25224415
+
+		Bubble Sort Aggressive took 0.65 secs, complexity was 25119051
+#######################################
+
+#######################################
+
+		Selection Sort O(n^2) = 100000000 
+
+
+		Selection Sort took 0.25 secs, complexity was 10000
+#######################################
+
+#######################################
+
+		Quick Sort  O( n log n) = 92103.403720 up to O(n^2) = 100000000 
+
+
+		Quick Sort took 0.00 secs, complexity was 33541
+#######################################
  */
 
 
@@ -9,108 +51,160 @@
 #include <stdlib.h>
 #include <string.h> // for memset/bzero utilities
 #include <time.h>   // for getting times
+#include <math.h>   // for log functions
 
 
 // number of items in the array 
 // to sort
-#define MAX_ITEMS 10
+#define MAX_ITEMS 10000
+
+#define MICROSECS_PER_SECOND 1000000
+
+#define BUBBLE_SORT            1
+#define BUBBLE_SORT_AGGRESSIVE 2
+#define MERGE_SORT             3
+#define QUICK_SORT             4
+#define SELECTION_SORT         5
 
 
 
-long
+double
 duration( struct timeval *startTime, struct timeval *endTime );
 
 void 
 init_array_with_random_values( int* arrayToFill, int count );
 
 void
-_dump( int * arrayToDump );
+_dump( int * arrayToDump, int end );
 
-int
-do_bubble_sort( int* array, int count );
+long
+do_bubble_sort( int* array, int start, int end );
 
-int
-do_bubble_sort_aggressive( int* array, int count );
+long
+do_bubble_sort_aggressive( int* array, int start, int end );
 
-int
-do_selection_sort( int* array, int count );
+long
+do_selection_sort( int* array, int start, int end );
 
-int
+long
 do_quick_sort( int* array, int start, int end );
 
+long
+do_merge_sort( int* array, int start, int end );
+
+void
+do_merge( int* array, int start, int partitionIndex, int end );
+
+void 
+run_sort( int whichAlghoritm, int* array_to_sort, int start, int end );
 
 ////////////////////////////////////////////////////////////////////////////////
 
 int
 main( int argc, char** argv ){
   
-  printf( "\nWelcome to the sorting sample!\n" );
-  printf( "\nThe program is configured for sorting %ld random values\n", (long) MAX_ITEMS );
+  long number_of_elements = 0;
 
-  int complexity = 0;
-  // variables to handle timing of the sorting alghoritms
-  struct timeval *startTime, *endTime;
-  startTime = (struct timeval *) malloc( sizeof( struct timeval ) );
-  endTime   = (struct timeval *) malloc( sizeof( struct timeval ) );
+
+  printf( "\nWelcome to the sorting sample!\n" );
+  printf( "\nThe program is configured for sorting max %ld random values\n", (long) MAX_ITEMS );
+
+  // get the number of elements to sort from the command line
+  if( argc == 2 )
+     number_of_elements = atol( argv[1] );
+  else
+    number_of_elements  = MAX_ITEMS;
+
 
 
   // create an integer array
   // with the number of items
-  int* array_to_sort = (int*) malloc( sizeof( int ) * MAX_ITEMS );
+  int* array_to_sort = (int*) malloc( sizeof( int ) * number_of_elements );
 
   // zero fill the array
-  memset( array_to_sort, 0, sizeof( int ) * MAX_ITEMS );
+  memset( array_to_sort, 0, sizeof( int ) * number_of_elements );
   // or this is equivalent
-  bzero( array_to_sort, sizeof( int ) * MAX_ITEMS );
+  bzero( array_to_sort, sizeof( int ) * number_of_elements );
 
 
-  init_array_with_random_values( array_to_sort, MAX_ITEMS );
+
+  run_sort( MERGE_SORT, array_to_sort, 0, number_of_elements );
+
+
+}
+
+
+void run_sort( int whichAlghoritm, int* array_to_sort, int start, int end ){
+
+  // pointer to the sorter engine
+  long (*sorter)(int*, int, int);
+  char *title  = NULL;
+  int number_of_elements = end - start;
+  long complexity         = 0;
+  // variables to handle timing of the sorting alghoritms
+  struct timeval *startTime, *endTime;
+  // allocate time structures
+  startTime = (struct timeval *) malloc( sizeof( struct timeval ) );
+  endTime   = (struct timeval *) malloc( sizeof( struct timeval ) );
+
+
+  switch( whichAlghoritm ){
+  case BUBBLE_SORT:
+    sorter = do_bubble_sort;
+    title  = "Bubble Sort O(n^2)";
+    break;
+  case BUBBLE_SORT_AGGRESSIVE:
+    sorter = do_bubble_sort_aggressive;
+    title  = "Bubble Sort Aggressive";
+    break;
+  case MERGE_SORT:
+    sorter = do_merge_sort;
+    title  = "Merge Sort O(n log n)";
+    break;
+  case QUICK_SORT:
+    sorter = do_quick_sort;
+    title  = "Quick Sort O(n log n)";
+    break;
+  case SELECTION_SORT:
+    sorter = do_selection_sort;
+    title  = "Selection Sort O(n^2)";
+    break;
+  default:
+    sorter = NULL;
+    printf( "\nNo sorter specified!\n" );
+    return;
+  }
+
+
   printf( "\n#######################################\n" );
-  printf( "\n\t\tBubble Sort O(n^2) = %ld \n", ( (long) MAX_ITEMS ) * MAX_ITEMS  );
+  printf( "\n\t\t" );
+  printf( title );
+	  
+  init_array_with_random_values( array_to_sort, number_of_elements );
+#ifdef SORT_VERBOSE
+  printf( "\n Initial array is\n" );
+  _dump( array_to_sort, number_of_elements );
+#endif // SORT_VERBOSE 
+
   gettimeofday( startTime, NULL );
-  complexity = do_bubble_sort( array_to_sort, MAX_ITEMS );
+  complexity = sorter( array_to_sort, start, number_of_elements );
   gettimeofday( endTime, NULL );
-  printf( "\nBubble Sort took %ld secs, complexity was %d", 
+  printf( "\n\n\t\t" );
+  printf( title );
+  printf( " %2.2f secs, complexity was %ld", 
 	  duration( startTime, endTime ),
 	  complexity );
-  init_array_with_random_values( array_to_sort, MAX_ITEMS );
-  gettimeofday( startTime, NULL );
-  complexity = do_bubble_sort_aggressive( array_to_sort, MAX_ITEMS );
-  gettimeofday( endTime, NULL );
-  printf( "\nBubble Sort Aggressive took %ld secs, complexity was %d", 
-	  duration( startTime, endTime ),
-	  complexity );
-  
+#ifdef SORT_VERBOSE
+  printf( "\nSorted array is\n" );
+  _dump( array_to_sort, number_of_elements );
+#endif //SORT_VERBOSE
+
   printf( "\n#######################################\n" );
 
-
-  init_array_with_random_values( array_to_sort, MAX_ITEMS );
-  printf( "\n#######################################\n" );
-  printf( "\n\t\tSelection Sort O(n^2) = %ld \n", ( (long) MAX_ITEMS ) * MAX_ITEMS  );
-  gettimeofday( startTime, NULL );
-  complexity = do_selection_sort( array_to_sort, MAX_ITEMS );
-  gettimeofday( endTime, NULL );
-  printf( "\n\t\ttook %ld secs, complexity was %d", 
-	  duration( startTime, endTime ),
-	  complexity );
-  printf( "\n#######################################\n" );
-
-
-  init_array_with_random_values( array_to_sort, MAX_ITEMS );
-  printf( "\n#######################################\n" );
-  printf( "\n\t\tQuick Sort  O( n log n) up to O(n^2) = %ld \n", ( (long) MAX_ITEMS ) * MAX_ITEMS  );
-  gettimeofday( startTime, NULL );
-  complexity = do_quick_sort( array_to_sort, 0, MAX_ITEMS );
-  gettimeofday( endTime, NULL );
-  printf( "\n\t\ttook %ld secs, complexity was %d", 
-	  duration( startTime, endTime ),
-	  complexity );
-  printf( "\n#######################################\n" );
-
-
-
-  _dump( array_to_sort );
-
+  /*  
+  free( startTime );
+  free( endTime );
+  */
 }
 
 
@@ -121,10 +215,11 @@ main( int argc, char** argv ){
  * \param endTime a pointer to the end time struct
  * \returns the number of elapsed seconds
  */
-long
+double
 duration( struct timeval *startTime, struct timeval *endTime ){
-  long secs = endTime->tv_sec - startTime->tv_sec;
-  secs     += ( endTime->tv_usec - startTime->tv_usec ) / 1000;
+  double secs = ( endTime->tv_sec - startTime->tv_sec ) * MICROSECS_PER_SECOND;
+  secs       += ( endTime->tv_usec - startTime->tv_usec );
+  secs       /= MICROSECS_PER_SECOND;
   return secs;
 }
 
@@ -138,7 +233,11 @@ void init_array_with_random_values( int* arrayToFill, int count ){
   int i = 0;
 
   for( i = 0; i < count; i++ )
+#ifndef SORT_SIMPLE
     arrayToFill[ i ] = (int) random();
+#else
+    arrayToFill[ i ] = 10 - i;
+#endif //SORT_SIMPLE
 }
 
 
@@ -152,9 +251,9 @@ void init_array_with_random_values( int* arrayToFill, int count ){
  * \returns the number of steps performed to sort the
  * array
  */
-int
-do_bubble_sort( int* array, int count ){
-  int complexity     = 0;
+long
+do_bubble_sort( int* array, int start, int end ){
+  long complexity     = 0;
   int swap_counter   = 0;
   int swapping_value;
   int i;
@@ -162,7 +261,7 @@ do_bubble_sort( int* array, int count ){
   do{
     swap_counter = 0;
 
-    for( i = 0; i < count - 1; i++ )
+    for( i = start; i < end - 1; i++ )
       if( array[ i ] > array[ i + 1 ] ){
 	// need a swap here
 	swapping_value = array[ i ];
@@ -194,12 +293,12 @@ do_bubble_sort( int* array, int count ){
  * \returns the number of steps performed to sort the
  * array
  */
-int
-do_bubble_sort_aggressive( int* array, int count ){
-  int complexity            = 0;
+long
+do_bubble_sort_aggressive( int* array, int start, int end ){
+  long complexity           = 0;
   int swap_counter          = 0;
   int biggest_swapped_index = 0;
-  int stopAtItem            = count - 1;
+  int stopAtItem            = end - 1;
   int swapping_value;
   int i;
 
@@ -208,7 +307,7 @@ do_bubble_sort_aggressive( int* array, int count ){
 
     biggest_swapped_index = 0;
 
-    for( i = 0; i < stopAtItem; i++ )
+    for( i = start; i < stopAtItem; i++ )
       if( array[ i ] > array[ i + 1 ] ){
 	// need a swap here
 	swapping_value = array[ i ];
@@ -239,22 +338,28 @@ do_bubble_sort_aggressive( int* array, int count ){
  * 4) swap the value with the first element
  * 5) repeat for every value in the list
  */
-int
-do_selection_sort( int* array, int count ){
-  int complexity         = 0;
+long
+do_selection_sort( int* array, int start, int end ){
+  long complexity         = 0;
   int current_min_index  = 0;
   int swapping           = 0;
   int i;
-  int start_index, end_index;
+  int start_index;
+  start_index = 0 + start;
 
   do{
     current_min_index = start_index;
 
-    for( i = start_index; i < count; i++ )
+    for( i = start_index; i < end; i++ )
       if( array[ current_min_index ] > array[ i ] )
 	// the i-th position is the new current min
 	current_min_index = i;
     
+
+#ifdef SORT_VERBOSE
+    printf( "\n\t[doSelectionSort] min index is %d, swapping value with index %d",
+	    current_min_index, start_index );
+#endif // SORT_VERBOSE
     
     // now I've got the min value in the list, place it at the
     // last position
@@ -265,16 +370,16 @@ do_selection_sort( int* array, int count ){
 
     complexity++;
     
-  }while( start_index < count );
+  }while( start_index < end );
 
   return complexity;
 }
 
 
 
-int
+long
 do_quick_sort( int* array, int start, int end ){
-  int complexity   = 0;
+  long complexity   = 0;
   int pivot_value  = 0;
   int pivot_index  = 0;
   int i;
@@ -287,6 +392,13 @@ do_quick_sort( int* array, int start, int end ){
   pivot_index = ( partition_end - partition_start ) / 2 + partition_start;
   pivot_value = array[ pivot_index ];
 
+
+#ifdef SORT_VERBOSE
+  printf( "\n\t[do_quick_sort] pivot value is %d at index %d - current partition from %d to %d ",
+	  pivot_value, pivot_index, start, end );
+#endif // SORT_VERBOSE
+
+
   // do the partitioning
   while( partition_start <= partition_end ){
 
@@ -297,6 +409,11 @@ do_quick_sort( int* array, int start, int end ){
       partition_end--;
 
     if( partition_start <= partition_end ){
+#ifdef SORT_VERBOSE
+      printf( "\n\t[do_quick_sort] swapping values at indexes %d <-> %d",
+	      partition_start, partition_end );
+#endif // SORT_VERBOSE
+
       swapping                 = array[ partition_start ];
       array[ partition_start ] = array[ partition_end ];
       array[ partition_end ]   = swapping;
@@ -306,6 +423,7 @@ do_quick_sort( int* array, int start, int end ){
       complexity++;
     }
   }
+
 
   // recursion
   if( start < partition_end )
@@ -319,10 +437,76 @@ do_quick_sort( int* array, int start, int end ){
 
 
 
+
+
+long
+do_merge_sort( int* array, int start, int end ){
+
+  int partitionIndex = ( end + start ) / 2;
+  long complexity    = 0;
+  
+  if( start < end ){
+#ifdef SORT_VERBOSE
+    printf( "\nAnalyzing partition split at index %d\n", partitionIndex );
+#endif // SORT_VERBOSE
+
+    do_merge_sort( array, start, partitionIndex );
+    do_merge_sort( array, partitionIndex + 1, end );
+
+#ifdef SORT_VERBOSE
+    printf( "\nDoing merge of %d <-> %d and %d <-> %d\n", 
+	    start, partitionIndex, partitionIndex + 1, end );
+#endif // SORT_VERBOSE
+
+    // do the merge
+    do_merge( array, start, partitionIndex, end );
+  }
+
+  return complexity;
+}
+
+
+void
+do_merge( int* array, int start, int partitionIndex, int end ){
+  int copyArray[ (end - start + 1 ) ];
+  int leftIndex  = start, rightIndex = partitionIndex + 1;
+  int copyIndex  = 0;
+  int mergeIndex = 0;
+
+  // sorting phase
+  while( leftIndex <= partitionIndex && rightIndex <= end )
+    if( array[ leftIndex ] < array[ rightIndex ] )
+      copyArray[ copyIndex++ ] = array[ leftIndex++ ];
+    else
+      copyArray[ copyIndex++ ] = array[ rightIndex++ ];
+
+  while( leftIndex <= partitionIndex )
+    copyArray[ copyIndex++ ] = array[ leftIndex++ ];
+
+  while( rightIndex <= end )
+      copyArray[ copyIndex++ ] = array[ rightIndex++ ];
+
+  // merge
+  for( mergeIndex = 0; mergeIndex < copyIndex; mergeIndex++ )
+    array[ start + mergeIndex ] = copyArray[ mergeIndex ];
+
+}
+
+
+
+
 ////////////////////////////////////////////////////////////////////////////////
 
-void _dump( int * array){
+void _dump( int * array, int count ){
   int i = 0;
-  for( i = 0; i < MAX_ITEMS; i++ )
-    printf( "\narray[ %d ] = %d", i, array[ i ] );
+
+  printf( "\n" );
+
+
+  for( i = 0; i < count; i++ ){
+
+    printf( "array[ %d ] = %d\n", i, array[ i ] );
+  }
+
+  printf( "\n" );
 }
