230b035aae2955a64a4283a46a2a59d2e2cb8c10
Luca Ferrari <fluca1978@gmail.com>
2012-10-04 08:21:23 +0200

    Placed a set of regular expressions to solve a few problems. Now the function accepts property names with a leading $ and spaces and remove them autonomously. Moreover, searching for the property, the getters and the setters is done with a regular expression and therefore there are better chance to get the right method.

--- emacs-lisp/fluca1978-php.el
+++ emacs-lisp/fluca1978-php.el
@@ -27,43 +27,43 @@
   (if (equal "" class-name)
       (message "Cannot create a class without a name!")
     (let* ((class-name (concat (capitalize (substring class-name 0 1) )
-					   (substring class-name 1) ) ) 
+			       (substring class-name 1) ) ) 
 	   (class-file-name (concat class-name ".php") ) ; the name of the file
 	   (class-edit-point 0)				 ; where to start to edit
 	   )
-	   ;; generate the buffer or visit it
-	   (switch-to-buffer (find-file-noselect class-file-name))
-	   ;; insert the beginning of the class
-	   (insert "<?php" ?\n
-		   ?\t "/* " class-file-name " */" ?\n
-		   ?\n ?\n
-		   "/**" ?\n
-		   " * " class-comment ?\n
-		   " */" ?\n
-		   "class " class-name " {" ?\n
-		   )
-	   (setq class-edit-point (point) ) ; store where the editing should start
+      ;; generate the buffer or visit it
+      (switch-to-buffer (find-file-noselect class-file-name))
+      ;; insert the beginning of the class
+      (insert "<?php" ?\n
+	      ?\t "/* " class-file-name " */" ?\n
+	      ?\n ?\n
+	      "/**" ?\n
+	      " * " class-comment ?\n
+	      " */" ?\n
+	      "class " class-name " {" ?\n
+	      )
+      (setq class-edit-point (point) ) ; store where the editing should start
 
-	   ;; insert the end of the class
-	   (insert ?\n ?\n ?\n
-		   "} /* end of the class */" ?\n
-		   ?\n ?\n
-		   "?>" ?\n
-		   )			; end of the insert block
+      ;; insert the end of the class
+      (insert ?\n ?\n ?\n
+	      "} /* end of the class */" ?\n
+	      ?\n ?\n
+	      "?>" ?\n
+	      )			; end of the insert block
 
-	   ;; indent the created region (whole buffer)
-	   (c-indent-region (point-min) (point-max) t)
+      ;; indent the created region (whole buffer)
+      (c-indent-region (point-min) (point-max) t)
 
-	   ;; place the cursor at the beginning of the class
-	   (goto-char class-edit-point)
+      ;; place the cursor at the beginning of the class
+      (goto-char class-edit-point)
 
-	   (message "Generation of the class done, start editing at line %d (char %d)" 
-		    (count-lines 1 class-edit-point)
-		    class-edit-point)
+      (message "Generation of the class done, start editing at line %d (char %d)" 
+	       (count-lines 1 class-edit-point)
+	       class-edit-point)
 
-	   )					; end of the let
+      )					; end of the let
     )						; end of the if
-)					; end of defun
+  )					; end of defun
 
 
 ;; -----------------------------------------------------------------------
@@ -76,123 +76,141 @@
    the cursor is"
   ;; prompt the user for the data to insert
   (interactive "sProperty name: \nsOptional comment for the property: ")
-  (if (equal "" property-name) 
-      ;; the user has not specified the property name!
-      (message "Cannot insert a not specified property, aborting!")
-    ;; if here the user has specified the property name, so
-    ;; compute the names of getter/setter
-    (let ((setter-name (concat "set" 
-			       (capitalize (substring property-name 0 1) )
-			       (substring property-name 1) )
-		       ) 		; end of the setter-name variable
-	  (getter-name (concat "get" 
-			       (capitalize (substring property-name 0 1) )
-			       (substring property-name 1) )
-		       ) 		; end of the getter-name variable
+  (progn
+    ;; ensure that the property name is correct, that is not an empty
+    ;; string and remove any leading $sign (the user could have typed $myProperty
+    ;; instead of myProperty)
+    (while (string-match "[\$ \s\t]^*" property-name)
+      (setq property-name (replace-match "" nil nil property-name))
+      )
+    (while (string-match "[ \s\t]$*" property-name)
+      (setq property-name (replace-match "" nil nil property-name))
+      )
+    (if (equal "" property-name) 
+	;; the user has not specified the property name!
+	(message "Cannot insert a not specified property, aborting!")
+      ;; if here the user has specified the property name, so
+      ;; compute the names of getter/setter
+      (let ((setter-name (concat "set" 
+				 (capitalize (substring property-name 0 1) )
+				 (substring property-name 1) )
+			 ) 		; end of the setter-name variable
+	    (getter-name (concat "get" 
+				 (capitalize (substring property-name 0 1) )
+				 (substring property-name 1) )
+			 ) 		; end of the getter-name variable
 
-	  (generate-getter t) 		; do I have to generate the getter?
-	  (generate-setter t) 		; do I have to generate the setter?
-	  (src-point (point)) 		; where am I?
-	  (property-insertion-point (point) ) ; the point where the property will be inserted
-	  (property-end-point (point) )	      ; the point where the property ends
-	  (generate-property t)
-	  (setter-arg-name (concat "$_" property-name ) )
-	  (method-insertion-point (point)) ;where the methods will be inserted
-	  ) 				; end of the let variable list
+	    (generate-getter t) 		; do I have to generate the getter?
+	    (generate-setter t) 		; do I have to generate the setter?
+	    (src-point (point)) 		; where am I?
+	    (property-insertion-point (point) ) ; the point where the property will be inserted
+	    (property-end-point (point) )	      ; the point where the property ends
+	    (generate-property t)
+	    (setter-arg-name (concat "$" property-name ) )
+	    (method-insertion-point (point)) ;where the methods will be inserted
+	    ) 				; end of the let variable list
 
-      ;; remember the current cursor position in the buffer
-      (save-excursion)
-
-      ;; I need to check if the getters and setters are already there
-      (goto-char (point-min) )
-      (search-forward "class")		; move to the beginning of the class
-      (if (search-forward setter-name nil t 1)
-	  (setq generate-setter nil)
-	(setq generate-setter t)
-	)
-      (goto-char (point-min) )
-      (if (search-forward getter-name nil t 1)
-	  (setq generate-getter nil)
-	(setq generate-getter t)
-	)
-      (goto-char (point-min) )
-      (if (search-forward (concat "$" property-name) nil t 1)
-			  (progn
-			    (setq generate-property nil)
-			    (setq property-insertion-point (point))
-			    )
-			  (setq generate-property t)
-	  )
-
-      ;; go back to the original position
-      (goto-char src-point)
-   
-      ;; do I have to generate the property?
-      (if generate-property
+	;; remember the current cursor position in the buffer
+	(save-excursion
 	  (progn
-	    (insert ?\n ?\t
-		    "/**" ?\n
-		    " * " property-comment ?\n
-		    "*/" ?\n
-		    "private $" property-name " = null;" ?\n
-		    ?\n ?\n
-		    )			; end of the insert for the property
-	    (setq property-end-point (point) ) ; store where the property ends
-	    ;; indent the property region
-	    (c-indent-region property-insertion-point property-end-point t)
 
+	    ;; I need to check if the getters and setters are already there
+	    (goto-char (point-min) )
+	    (search-forward "class")		; move to the beginning of the class
+	    (if (re-search-forward (concat "[\s \t\n]*public[\s \t\n]*function[\s \t\n]*" setter-name "[\s \t\n]*\(.*\)") nil t 1)
+		(setq generate-setter nil)
+	      (setq generate-setter t)
+	      )
+	    (goto-char (point-min) )
+	    (if (re-search-forward (concat "[\s \t\n]*public[\s \t\n]*function[\s \t\n]*" getter-name "[\s \t\n]*\([\s \t\n]*\)") nil t 1)
+		(setq generate-getter nil)
+	      (setq generate-getter t)
+	      )
+	    (goto-char (point-min) )
+	    (if (re-search-forward (concat "[\s \t\n]* \\(private\\|public\\)[\s \t\n]*$" property-name) nil t 1)
+		(progn
+		  (setq generate-property nil)
+		  (setq property-insertion-point (point))
+		  )
+	      (setq generate-property t)
+	      )
 
-	    ;; go to the end of the buffer and position before the last
-	    ;; curly brace that should close the class
-	    (goto-char (point-max))		; goto end of buffer
-	    (re-search-backward "}")		; go back to the last }
-	    (setq method-insertion-point (point) ) ; store where the methods will be added
+	    ;; go back to the original position
+	    (goto-char src-point)
 	    
-	    (if generate-getter 
-		(insert ?\n ?\t
-			"/**" ?\n
-			"* Getter for the property " property-name "." ?\n
-			"* \\returns the current value of the property " property-name ?\n
-			"*/" ?\n
-			"public function " getter-name "(){"
-			?\n ?\t 
-			"return $this->" property-name ";"
-			?\n ?\t
-			"}"
-			?\n ?\n ?\n
-			)		; end of the insert for the getter
-	      (message "Skipping the generation of the getter method")
-	      )			; end of the if for the getter method
-	    (if generate-setter
-		(insert
-		 ?\n ?\t 
-		 "/**" ?\n
-		 "* Setter of the property " property-name "." ?\n
-		 "* \\param " setter-arg-name " the new value for the property" ?\n
-		 "*/" ?\n
-		 "public function " setter-name "( " setter-arg-name " ){"
-		 ?\n ?\t 
-		 "$this->" property-name " = " setter-arg-name ";" 
-		 ?\n ?\t
-		 "}"
-		 ?\n ?\n ?\n
-		 )			; end of the insert body for the setter method
-	      (message "Skipping the generation of the setter method")
-	      )				; end of the if for the setter method
+	    ;; do I have to generate the property?
+	    (if generate-property
+		(progn
+		  (insert ?\n ?\t
+			  "/**" ?\n
+			  " * " property-comment ?\n
+			  "*/" ?\n
+			  "private $" property-name " = null;" ?\n
+			  ?\n ?\n
+			  )			; end of the insert for the property
+		  (setq property-end-point (point) ) ; store where the property ends
+		  ;; indent the property region
+		  (c-indent-region property-insertion-point property-end-point t)
+
+
+		  ;; go to the end of the buffer and position before the last
+		  ;; curly brace that should close the class
+		  (goto-char (point-max))		; goto end of buffer
+		  (re-search-backward "}")		; go back to the last }
+		  (setq method-insertion-point (point) ) ; store where the methods will be added
+		  
+		  (if generate-getter 
+		      (insert ?\n ?\t
+			      "/**" ?\n
+			      "* Getter for the property " property-name "." ?\n
+			      "* \\returns the current value of the property " property-name ?\n
+			      "*/" ?\n
+			      "public function " getter-name "(){"
+			      ?\n ?\t 
+			      "return $this->" property-name ";"
+			      ?\n ?\t
+			      "}"
+			      ?\n ?\n ?\n
+			      )		; end of the insert for the getter
+		    (message "Skipping the generation of the getter method")
+		    )			; end of the if for the getter method
+		  (if generate-setter
+		      (insert
+		       ?\n ?\t 
+		       "/**" ?\n
+		       "* Setter of the property " property-name "." ?\n
+		       "* \\param " setter-arg-name " the new value for the property" ?\n
+		       "*/" ?\n
+		       "public function " setter-name "( " setter-arg-name " ){"
+		       ?\n ?\t 
+		       "$this->" property-name " = " setter-arg-name ";" 
+		       ?\n ?\t
+		       "}"
+		       ?\n ?\n ?\n
+		       )			; end of the insert body for the setter method
+		    (message "Skipping the generation of the setter method")
+		    )				; end of the if for the setter method
+		  
+
+		  ;; indent the region for the methods
+		  (c-indent-region method-insertion-point (point) t)
+
+		  ) 				; end of the progn on the generation of the property
+
+	      (message "Cannot generate the property [%s], it already exists at line %d" 
+		       property-name 
+		       (count-lines 1 property-insertion-point ) )
+	      )				; end of the if for the property generation
 	    
 
-	    ;; indent the region for the methods
-	    (c-indent-region method-insertion-point (point) t)
+	    ) 					; end of progn within save excursion
+	  )					; end of the save-escurion
+	)						; end of the let main body
 
-	    ) 				; end of the progn on the generation of the property
-
-	(message "Cannot generate the property [%s], it already exists at line %d" 
-		 property-name 
-		 (count-lines 1 property-insertion-point ) )
-	)				; end of the if for the property generation
-   
-
-      ) 					; end of the let main body
-    )					;end of the if on the property name
+      )					;end of the if on the property name
+    )					; end of the progn
   )				;end of defun
+
+
+
 
