cbdeebe336ca546bdb424e647f09ca858645c649
Luca Ferrari <fluca1978@gmail.com>
2012-10-08 14:00:34 +0200

    Rewritten the engine to place property and getter/setter into the header file.

--- emacs-lisp/fluca1978-qt.el
+++ emacs-lisp/fluca1978-qt.el
@@ -66,126 +66,260 @@
 	    (file-name-header nil)			     ; the header (.h) file name
 	    (insert-methods-in-header nil)		     ; true if the getter/setter have to be inserted
 					                     ; in the header file
-	    (class-name "Luca")				     ; the name of the class the property will be inserted into
+	    (class-name nil)				     ; the name of the class the property will be inserted into
+	    (generate-private-section nil)		     ; do I have to generate the private section in the class definition?
+	    (generate-public-section  nil)		     ; do I have to generate the public section?
+	    (visiting-buffer-filename nil)		     ; the buffer the user has invoked this function onto
+	    (visiting-point           nil)		     ; where the user was editing
+	    (class-beginning-point    nil)		     ; where the class starts in the header
 	    ) 				; end of the let variable list
 
 	;; remember the current cursor position in the buffer
 	(save-excursion
 	  (progn
 
-	    ;; I need to check if the getters and setters are already there
-	    (goto-char (point-min) )
-	    (search-forward "class")		; move to the beginning of the class
-	    (if (re-search-forward (concat "[\s \t\n]*void[\s \t\n]*" setter-name "[\s \t\n]*\(.*\)") nil t 1)
-		(setq generate-setter nil)
-	      (setq generate-setter t)
-	      )
-	    (goto-char (point-min) )
-	    (if (re-search-forward (concat "[\s \t\n]*" property-type "[\s \t\n]*" getter-name "[\s \t\n]*\([\s \t\n]*\)") nil t 1)
-		(setq generate-getter nil)
-	      (setq generate-getter t)
-	      )
-	    (goto-char (point-min) )
-	    (if (re-search-forward (concat "[\s \t\n]*" property-name) nil t 1)
+	    ;; I need to try to understand what kind of buffer I am visiting:
+	    ;; if it is an header file, than it must contain the class declaration
+	    (setq visiting-buffer-filename (buffer-file-name) )
+	    (if (string-match "[.]*h[pp]*" (file-name-extension visiting-buffer-filename) )
+		;; on the header file, so search for the class name
 		(progn
+		  (setq visiting-point (point))
+		  (goto-char (point-min))
+		  (setq insert-methods-in-header t) ; insert the methods into the header
+
+		  (if (re-search-forward "class[ \t\s\n]+\\(.+\\)[ \t\s\n]*\\(.*\\)[ \t\s\n]*[{]$" 
+					 nil t 1 )
+		      (progn
+			;; the user is editing the header file, so I guess
+			;; she wants to insert the getter/setter into the header file
+			;; itself
+			(setq class-name "LUCA")
+			(setq insert-methods-in-header t)
+			(setq class-beginning-point (point))
+
+
+
+			
+
+			;; search for the private section
+			(goto-char class-beginning-point)
+			(if (re-search-forward "[\s \t\n]*private:[\s \t\n]*" nil t 1)
+			    (progn
+			      (setq generate-private-section nil)
+			      (setq property-insertion-point (point))
+			      )
+
+			  (progn
+			    (setq generate-private-section t)
+			    (setq property-insertion-point class-beginning-point)
+			    )
+			  )
+
+		    
+		    )
+		  )
+	      
+
+	    ;; if the getter/setter have to be inserted in the class
+	    ;; implementation (i.e., cpp file), their name must have the
+	    ;; class qualifier
+
+	       (unless (equal insert-methods-in-header t) 
+		 (unless (null class-name)
+		   (progn
+		     
+		     (setq getter-name (concat class-name "::" getter-name) )
+		     (setq setter-name (concat class-name "::" setter-name) )
+		     )
+		   )
+		 )
+
+
+	    ;; I need to check if the property has been already defined in the
+	    ;; header file (under the private section)
+
+	    (goto-char class-beginning-point)	; move to the beginning of the class
+	    (if (re-search-forward (concat "[\s \t\n]*"  property-name) nil t 1)
+		(progn
+		  (goto-char (point-max) )
+		  (search-backward "}")
 		  (setq generate-property nil)
 		  (setq property-insertion-point (point))
 		  )
 	      (setq generate-property t)
 	      )
 
-	    ;; go back to the original position
-	    (goto-char src-point)
 
-	    ;; if the getter/setter have to be inserted in the class
-	    ;; implementation (i.e., cpp file), their name must have the
-	    ;; class qualifier
-	    (unless (equal insert-methods-in-header t) 
-	      (unless (null class-name)
-		(progn
-		  (setq getter-name (concat class-name "::" getter-name) )
-		  (setq setter-name (concat class-name "::" setter-name) )
-		  )
-		)
-	      )
-
-	    
 	    ;; do I have to generate the property?
-	    (if generate-property
+	    (unless (null generate-property )
 		(progn
+		  ;;  first of all go to where the property has to be placed
+		  (goto-char property-insertion-point)
+
+		  ;;  do I have to generate the private section? If so go to the end
+		  ;;  of the class to avoid making privates variable
+		  (if generate-private-section 
+		      (progn			
+			(insert ?\n ?\t "private:" ?\n)
+			)
+		    )
+		  
 		  (insert ?\n ?\t
 			  "/**" ?\n
 			  " * " property-comment ?\n
 			  "*/" ?\n
 			  property-type " " property-name ";" ?\n
-			  getter-name
 			  ?\n ?\n
 			  )			; end of the insert for the property
 		  (setq property-end-point (point) ) ; store where the property ends
 		  ;; indent the property region
 		  (c-indent-region property-insertion-point property-end-point t)
+		  ;; here the property has been inserted in the header file/buffer, save it
+		  (save-buffer)
+		  )
+
+		)					; end of null generate
 
 
-		  ;; go to the end of the buffer and position before the last
-		  ;; curly brace that should close the class
-		  (goto-char (point-max))		; goto end of buffer
-		  (re-search-backward "}")		; go back to the last }
-		  (setq method-insertion-point (point) ) ; store where the methods will be added
-		  
-		  (if generate-getter 
+	    ;; now see if I need to generate the methods into the header file
+	    (unless (null insert-methods-in-header)
+	      (progn
+		
+		;; does the header include a public section?
+		;; assume that the getter/setter method position will be
+		;; right after the public section
+		(goto-char class-beginning-point)
+		(if (re-search-forward "[\s \t\n]*public:[\s \t\n]*" nil t 1)
+		    (progn
+		      (setq generate-public-section nil)
+		      (setq method-insertion-point (point))
+		      )
+		  (progn
+		    (setq generate-public-section t)
+		    (goto-char (point-max))
+		    (search-backward "}")
+		    (setq method-insertion-point (point))
+		    )
+		  )
 
-		      (insert ?\n ?\t
-			      "/**" ?\n
-			      "* Getter for the property " property-name "." ?\n
-			      "* \\returns the current value of the property " property-name ?\n
-			      "*" ?\n
-			      "* \\author " user-full-name " - " user-mail-address ?\n
-			      "*/" ?\n
-			      property-type ?\n getter-name "(){"
-			      ?\n ?\t 
-			      "return this->" property-name ";"
-			      ?\n ?\t
-			      "}"
-			      ?\n ?\n ?\n
-			      )		; end of the insert for the getter
-		    (message "Skipping the generation of the getter method")
-		    )			; end of the if for the getter method
-		  (if generate-setter
-		      
-		      (insert
-		       ?\n ?\t 
-		       "/**" ?\n
-		       "* Setter of the property " property-name "." ?\n
-		       "* \\param " setter-arg-name " the new value for the property" ?\n 
-		       "*" ?\n
-		       "* \\author " user-full-name " - " user-mail-address ?\n
-		       "*/" ?\n
-		       "void" ?\n setter-name "( " property-type " " setter-arg-name " ){"
-		       ?\n ?\t 
-		       "this->" property-name " = " setter-arg-name ";" 
-		       ?\n ?\t
-		       "}"
-		       ?\n ?\n ?\n
-		       )			; end of the insert body for the setter method
-		    (message "Skipping the generation of the setter method")
-		    )				; end of the if for the setter method
-		  
+		;; search for the getter/setter
+		(goto-char method-insertion-point)
+		(if (re-search-forward (concat "[ \s\n\t]*" property-type "[ \s\n\t]+" getter-name "[ \s\n\t]*([ \s\n\t]*)" ) nil t 1 )
+		    (setq generate-getter nil)
+		  (setq generate-getter t)
+		  )
+		(goto-char method-insertion-point)
+		(if (re-search-forward (concat "[ \s\n\t]*void[ \s\n\t]+" setter-name "[ \s\n\t]*([ \s\n\t]*" property-type "[ \s\n\t]+\\(.+\\)[ \s\n\t]*)" ) nil t 1 )
+		    (setq generate-setter nil)
+		  (setq generate-setter t)
+		  )
+				       
+				      
+		
 
-		  ;; indent the region for the methods
-		  (c-indent-region method-insertion-point (point) t)
+		(goto-char method-insertion-point)
 
-		  ) 				; end of the progn on the generation of the property
+		;;  do I have to generate the public section?
+		(if (equal generate-public-section t)
+		    (insert ?\t "public:" ?\n)
+		  )
 
-	      (message "Cannot generate the property [%s], it already exists at line %d" 
-		       property-name 
-		       (count-lines 1 property-insertion-point ) )
-	      )				; end of the if for the property generation
-	    
+		;; place the getter
+		(unless (null generate-getter)
+		  (qt-insert-accessor method-insertion-point property-name property-type t)
+		  )
+		
+		;; place the setter
+		(unless (null generate-setter)
+		  (qt-insert-accessor (point) property-name property-type nil)
+		  )
 
-	    ) 					; end of progn within save excursion
-	  )					; end of the save-escurion
-	)						; end of the let main body
+		)	      ; end of progn
+	      )				; end of unless insert methods in header
 
-      )					;end of the if on the property name
-    )					; end of the progn
-  )				;end of defun
+	      )
+	    )					; end of the case on the header file
+	    )						; end of the progn
+	  )						; end of save-excursion
+	)					; end of let
+	
+      
+      )					; end of the if on the property name
+    )					; end of the main progn
+  )					; end of the function
+
+
+
+
+;; A function to insert a getter/setter method at a specific point in the buffer.
+;; The property name will be cut off the first character, that is supposed to be
+;; a "_"; the is-getter is set to true to generate a getter method, nil to generate
+;; a setter method.
+;;
+;; The following calls are examples:
+;;
+;; (qt-insert-accessor (point) "_myProperty" "QObject*" t)
+;; (qt-insert-accessor (point) "_myProperty" "QObject*" nil)
+;;
+;;
+(defun qt-insert-accessor (method-insertion-point property-name property-type is-getter )
+
+  (let (
+	(setter-arg-name (substring property-name 1))
+	)
+      (if (equal is-getter t)
+	  (progn
+	    (goto-char method-insertion-point)
+
+	    ;; place the getter
+	    (insert ?\n ?\t
+		    "/**" ?\n
+		    "* Getter for the property " property-name "." ?\n
+		    "* \\returns the current value of the property " property-name ?\n
+		    "*" ?\n
+		    "* \\author " user-full-name " - " user-mail-address ?\n
+		    "*/" ?\n
+		    property-type ?\n getter-name "()"
+		    ?\n ?\t 
+		    "{" ?\n ?\t
+		    "return this->" property-name ";"
+		    ?\n ?\t
+		    "}"
+		    ?\n ?\n ?\n
+		    )		; end of the insert for the getter
+	    )
+	(progn
+	  (goto-char method-insertion-point)
+	  
+	  (insert
+	   ?\n ?\t 
+	   "/**" ?\n
+	   "* Setter of the property " property-name "." ?\n
+	   "* \\param " setter-arg-name " the new value for the property" ?\n 
+	   "*" ?\n
+	   "* \\author " user-full-name " - " user-mail-address ?\n
+	   "*/" ?\n
+	   "void" ?\n setter-name "( " property-type " " setter-arg-name " )"
+	   ?\n ?\t 
+	   "{" ?\n ?\t
+	   "this->" property-name " = " setter-arg-name ";" 
+	   ?\n ?\t
+	   "}"
+	   ?\n ?\n ?\n
+	   )			; end of the insert body for the setter method
+	  
+	  )				; end of the else case (setter genration)
+	)				; end of the if on the type of method
+    
+      ;; indent the region where the methods have been defined
+      (c-indent-region method-insertion-point (point) t)
+      
+
+    
+    
+
+
+    )  				; end of the let body
+  
+)
