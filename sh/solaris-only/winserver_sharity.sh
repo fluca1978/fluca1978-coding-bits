3864c19578a8032e30308c5611bc96d440dfd24a
Luca Ferrari <fluca1978@gmail.com>
2013-02-11 17:09:42 +0100

    A template file for using sharity to mount a Windows share on a Solaris 8 environment. The template has italian messages.

---
+++ sh/solaris-only/winserver_sharity.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+win_server=""
+win_share=""
+win_username=""
+win_password=""
+win_mount="/winserver"
+sharity_cmd="/usr/local/sharity3/bin/sharity"
+
+
+echo "=== Collegamento al server windows $win_server cartella di rete $win_share... ==="
+
+# dump dei parametri
+echo "=== Server: $win_server   ==="
+echo "=== Share:  $win_share    ==="
+echo "=== User:   $win_username ==="
+echo "=== Mount:  $win_mount    ==="
+
+# login al server
+su - luca -c "$sharity_cmd login smb://$win_server/$win_share -U $win_username -P $win_password"	        #
+if test $? -ne 0									        #
+then											        #
+    echo "=== ERRORE: impossibile fare login al server con le credenziali specificate! ==="  #
+    exit 1										        #
+fi											        #
+
+# effettuo il mount
+su - luca -c "$sharity_cmd mount smb://$win_server/$win_share $win_mount"
+if test $? -eq 0
+then
+    echo "=== Cartella collegata con successo e disponibile in $win_mount! ==="
+else
+    echo "=== ERRORE: impossibile montare la cartella di rete ==="
+    exit 2
+fi
