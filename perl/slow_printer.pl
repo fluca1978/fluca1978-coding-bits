c29ab4b83f64c799fa4e14e0f2c0cafa4b6eea5f
Luca Ferrari <fluca1978@gmail.com>
2016-11-17 17:04:15 +0100

    Implementing a movie-like slow printer

---
+++ perl/slow_printer.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+use v5.10;
+use Time::HiRes qw( usleep );
+
+open my $fh, $ARGV[0] || die "\nCannot open input file\n$!";
+$/ = \1; # set one char at time (input record separator)
+$| = 1;  # force autoflush
+while ( <$fh> ){
+    print;
+    usleep( ( /[a-zA-Z]/ ? 1 : 2  ) * 75000  );
+}
