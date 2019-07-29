e8e0f28d280e3504a0f95ed83edf1d2457b9ea1d
Luca Ferrari <fluca1978@gmail.com>
2019-07-29 10:03:00 +0200

    Simple script to convert org files into PDF presentations.

---
+++ sh/org2beamer2pdf.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+EMACS=$(which emacs)
+DIR=$1
+
+
+if [ -z "$EMACS" ]; then
+    echo "Emacs not found!"
+    exit 1
+fi
+
+
+
+if [ -z "$DIR" ]; then
+    DIR=$(cwd)
+fi
+
+if [ ! -d "$DIR" ]; then
+    echo "Please specify the directory to work in"
+    exit 2
+fi
+
+echo "Searching for org files in [$DIR]"
+cd $DIR
+for forg in *.org
+do
+    echo "Converting [$forg] ..."
+    $EMACS --batch $forg -f org-beamer-export-to-pdf > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "ok!"
+    else
+        echo "KO!"
+    fi
+done
